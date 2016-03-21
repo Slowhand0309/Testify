e4be7f6f5b8f421f226e697aba895655967ce91c
slowhand0309 <slowhand0309@gmail.com>
2016-03-21 17:43:58 +0900

    Added indexdoc_task, resdoc_task

---
+++ lib/recot/tasks/indexdoc_task.rb
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+module Recot
+  module Tasks
+    # task for update index.html
+    class IndexdocTask < BaseTask
+    end
+  end
+end
---
+++ lib/recot/tasks/resdoc_task.rb
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+module Recot
+  module Tasks
+    # generate resources html file
+    class ResdocTask < BaseTask
+    end
+  end
+end
