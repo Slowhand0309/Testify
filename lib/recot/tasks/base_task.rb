f1ab3ba806eed79e8a08e23d08878e4977a331db
slowhand0309 <slowhand0309@gmail.com>
2016-03-25 00:37:16 +0900

    Updated resdoc task

--- lib/recot/tasks/base_task.rb
+++ lib/recot/tasks/base_task.rb
@@ -14,17 +14,25 @@ module Recot
 
 protected
 
-      # Get current evidence path.
+      # Get Current test no.
       #
       # == Returns:
-      # A string of evidence path.
+      # Number of test.
       # If not cached test no return empty string.
-      def evidence_path()
+      def get_testno()
         test_no = Recot::Cache::StateCache.restore_no
         unless test_no
           return "" # return empty string
         end
-        "#{Recot.resources_dir}/#{test_no}"
+        test_no
+      end
+
+      # Get current evidence path.
+      #
+      # == Returns:
+      # A string of evidence path.
+      def evidence_path()
+        "#{Recot.resources_dir}/#{get_testno}"
       end
     end
   end
--- lib/recot/tasks/resdoc_task.rb
+++ lib/recot/tasks/resdoc_task.rb
@@ -12,8 +12,8 @@ module Recot
       # == Parameters:
       # Arguments of resource files.
       def run(args)
-        unless valid_args?(args)
-          $stderr.puts "invalid argument #{args}"
+        unless valid_state?(args)
+          $stderr.puts "invalid state. files #{args}"
         else
           generate(args)
         end
@@ -26,21 +26,21 @@ private
       # == Parameters:
       # Arguments of resource files.
       def generate(args)
-        args.each do |f|
-          # Set the data hash object.
-          data = {}
-          # Create evidence file path
-          base_name = File.basename(f)
-          data[:base_name] = base_name
-          data[:title] = base_name
 
-          # Path of evidence
-          path = "#{evidence_path}/#{base_name}"
-          data[:evidence_path] = path
-
-          # Render
-          Recot::Render::HtmlRender.new.render_resdoc(data, resdoc_path)
+        # Get resource files.
+        files = []
+        Dir::glob("#{evidence_path}/*").each do |f|
+          # Create evidence file path.
+          files << f
         end
+
+        # Set the data hash object.
+        data = {}
+        data[:title] = get_testno()
+        data[:files] = files
+
+        # Render
+        Recot::Render::HtmlRender.new.render_resdoc(data, resdoc_path)
       end
 
       def gen_resdir
@@ -52,22 +52,20 @@ private
       #
       # == Returns:
       # A path of resdoc. If not cached return 'unkown.html'.
-      def resdoc_path
-        test_no = Recot::Cache::StateCache.restore_no
-        test_no ||= 'unkown'
-
-        "#{Recot.resources_dir}/#{test_no}.html"
+      def resdoc_path()
+        "#{evidence_path}.html"
       end
 
-      # Check args valid.
+      # Check args and test no.
       #
       # == Parameters:
       # Arguments array object.
       #
       # == Returns:
-      # True if valid argument.
-      def valid_args?(args)
-        args && args.size > 0
+      # True if valid state.
+      def valid_state?(args)
+        no = test_no = Recot::Cache::StateCache.restore_no
+        no && args && args.size > 0
       end
     end
   end
