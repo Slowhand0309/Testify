8bf6ba8c007a5330e9754f02de94e14607773fd0
Slowhand0309 <slowhand0309@gmail.com>
2016-06-22 06:30:34 +0900

    Added export util class

---
+++ lib/recot/utils/export_util.rb
@@ -0,0 +1,20 @@
+# coding: utf-8
+require 'axlsx'
+
+module Recot
+  module Utils
+    class ExportUtil
+
+      class << self
+
+        # Export spreadsheet.
+        #
+        # == Parameters:
+        # A spreadsheet file name
+        def to_spreadsheet(filename)
+        end
+      end
+
+    end
+  end
+end
